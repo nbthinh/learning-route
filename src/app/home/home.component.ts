06271d8b5e0b987bbd571572cee968905c8c476a
nbthinh <nguyenbuuthinh036@gmail.com>
2023-04-25 06:29:41 +0700

    test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { from, Observable, of } from "rxjs";
 import { Article } from "../models/article";
 import { ArticleService } from "../services/article.service";
@@ -10,7 +11,9 @@ import { ArticleService } from "../services/article.service";
       <ul>
         <li *ngFor="let article of articles$ | async" style="border: 1px solid black; padding: 20px; margin-bottom: 10px;">
           {{ article.title }} <br/>
-          <a href="#">Read more</a>
+          <!-- <a [routerLink]="['/detail', article.slug]">Read more</a> -->
+          <a style="cursor: pointer; text-decoration: underline;" (click)="onReadMoreClick(article.slug)">Read more</a>
+
         </li>
       </ul>
       <!-- <router-outlet></router-outlet> -->
@@ -18,8 +21,11 @@ import { ArticleService } from "../services/article.service";
 })
 export class HomeComponent implements OnInit {
   articles$!: Observable<Article[]>;
-  constructor(private readonly articleService: ArticleService) {}
+  constructor(private readonly articleService: ArticleService, private readonly router: Router) {}
   ngOnInit(): void {
     this.articles$ = this.articleService.article$;
+  }
+  onReadMoreClick(slug: string){
+    this.router.navigate(['/detail', slug])
   }
 }
